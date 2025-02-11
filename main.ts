ccbb7fd6922ec8bea53090abf1cf2df65a88149f
NeverStopTheCoder <jenny_fullerton@yahoo.com>
2025-02-11 09:51:59 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,1 @@
-controller.menu.onEvent(ControllerButtonEvent.Pressed, function () {
-    slider.stopcontrolingslider()
-})
-let Slider = slider.createSlider3(2, 80, 8)
-slider.controlSlider3(Slider)
+
